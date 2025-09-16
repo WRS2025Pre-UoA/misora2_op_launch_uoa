11dd09d956048115096daabc223ff1608a5f6772
rui323 <ruiiwata837@gmail.com>
2025-09-16 14:11:21 +0900

    update P3 remap

--- launch/bringupP3.launch.py
+++ launch/bringupP3.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
                 name="misora_gui",
                 extra_arguments=[{"use_intra_process_comms": True}],
                 parameters=[{"mode": "P3"}],
-                remappings=[("raw_image" , "/arm_color_thermal_camera/selected_image_raw/compressed")]
+                remappings=[("raw_image" , "/arm_color_thermal_camera/selected_image_raw/compressed"),("raw_image_metal", "/metal_loss_camera/image_raw")]
             ),
         ],
         output="screen",
